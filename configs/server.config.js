32e9f1b3dd8a7b5e06ebff08b1e282aaa6de4dfb
Marvin <Ross Chadwick>
2016-10-13 14:13:01 +0200

    Config updates

--- configs/server.config.js
+++ configs/server.config.js
@@ -6,8 +6,8 @@ export default {
         'generic': {
             host: 'localhost', port: 8890, path: '/sparql', type: 'virtuoso'
         },
-        'http://localhost:5820/testDB/query': {
-            host: 'localhost', port: 5820, path: '/testDB/query', useDefaultGraph: 1, type: 'sesame', useReasoning: 1
+        'http://localhost:5820/votes/query': {
+            host: 'localhost', port: 5820, path: '/votes/query', useDefaultGraph: 1, type: 'sesame', useReasoning: 1
         }
     },
     dbpediaLookupService: [
